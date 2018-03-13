8b5e05d1cbd682b4a21668223a6a376e84c82bfd
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-03-13 21:48:26 +0100

    Make it work

--- js/shakespeare.js
+++ js/shakespeare.js
@@ -1,11 +1,11 @@
 var NASA_API_KEY = 'EQzgRUB1sMiKLrZo4kd6KPNbIgKB95FV9jaGtVQA'
 
-function getRoverImage(day) {
-    return fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=' + day + '&api_key=' + NASA_API_KEY)
+function getRoverImage(day, page) {
+    return fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?page=${page}&sol=${day}&api_key=` + NASA_API_KEY)
         .then(function (response) {
                 return response.json()
             }
-        )
+        ).catch(console.error)
 }
 
 function getShakespeareQuote(quoteName, successFunction, failureFunction) {
@@ -19,28 +19,31 @@ function getShakespeareQuote(quoteName, successFunction, failureFunction) {
 $(document).ready(function () {
 
     getShakespeareQuote("wordsworth", function (quote) {
-       $('#text').text(quote.quote);
-       $('#text').append("<br/>");
-       $('#text').append("<h4>"+quote.titles+"</h4>");
-                 console.log(quote);
-    }, 
-                function (error) {
-                console.log(error)
-    });
+            $('#text').text(quote.quote);
+            $('#text').append("<br/>");
+            $('#text').append("<h4>" + quote.titles + "</h4>");
+            console.log(quote);
+        },
+        function (error) {
+            console.log(error)
+        });
+    const randomDay =Math.floor(Math.random() * 1000) + 1
+    const randomPage =Math.floor(Math.random() * 10) + 1
 
-    getRoverImage().then(function (response) {
+    getRoverImage(randomDay, randomPage).then(function (response) {
 
-        for (i = 0; i <response.photos; i++) { 
+        for (var i = 0; i < response.photos.length; i++) {
+            console.log(i);
 
-        var photo =response.photos[i].img_src;
-        $('#bg-image').attr("src", photo);
-        $('body').append('img src="'+ photo + '"/>')
-        console.log(photo);
-
-
-        console.log(response);
+            var photo = response.photos[i].img_src;
+            $('#bg-image').attr("src", photo);
+            $('body').append(`<img class="bg-image" src="${photo}"/>`)
 
 
-    }
+            console.log(response);
 
-});
+
+        }
+
+    })
+})
